73b21f23f9ef659205a38a004ed9afa49cb617fc
karencfisher <karencfisher@hotmail.com>
2025-01-05 17:10:09 -0700

    updating dcgan model

--- dcgan.py
+++ dcgan.py
@@ -68,7 +68,7 @@ class DCGAN:
     def __discriminator(self, channels=1):
         discriminator = keras.Sequential(
             [
-                keras.Input(shape=self.input_shape),
+                keras.Input(shape=(64, 64, self.input_shape[-1])),
                 layers.Conv2D(64, kernel_size=4, strides=2, padding="same"),
                 layers.LeakyReLU(alpha=0.2),
                 layers.Conv2D(128, kernel_size=4, strides=2, padding="same"),
